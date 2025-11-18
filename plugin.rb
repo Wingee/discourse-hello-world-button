626080b4eb87b67fc09477c556fc295c5bc0e002
Wingee <engwei.chua@ingelby.com>
2025-11-18 17:45:21 +0000

    Updated

--- plugin.rb
+++ plugin.rb
@@ -3,7 +3,11 @@
 # about: Big red HELLO WORLD button
 # version: 1.0
 # authors: engwei + Grok
+# url:  https://github.com/Wingee/discourse-hello-world-button
 
-after_initialize do
-  # Ruby plugin code (if any) goes here
-end
+# register stylesheet (optional; SCSS under assets/stylesheets will be processed)
+register_asset "stylesheets/hello-world.scss"
+
+# NOTE: Do NOT register JS files under assets/javascripts.
+# Those are auto-included by Discourse's bundler. Registering them manually
+# causes boot failures (as you saw).
